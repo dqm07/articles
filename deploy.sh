3d4a71828c9861d29c1abe4975292758d7292340
dongqingming <dongqingming@didichuxing.com>
2021-04-07 21:37:35 +0800

    修复push脚本

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 ###
  # @Author: dongqingming
  # @Date: 2021-03-23 20:54:15
- # @LastEditTime: 2021-03-23 21:28:56
+ # @LastEditTime: 2021-04-07 21:35:58
  # @LastEditors: dongqingming
  # @Description: Do not edit
  # @FilePath: /articles/deploy.sh
@@ -14,7 +14,7 @@ npm run docs:build
 
 cd docs/.vuepress/dist
 
-# git init
+git init
 git add -A
 git commit -m 'deploy'
 git push -f https://github.com/dqm07/dqm07.github.io.git master
